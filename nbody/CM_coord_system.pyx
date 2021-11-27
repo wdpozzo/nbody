4c2e53dec0da47e9fd822e53700350cc4c897715
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2021-11-27 13:33:21 +0100

    fix input and cdef

--- nbody/CM_coord_system.pyx
+++ nbody/CM_coord_system.pyx
@@ -1,19 +1,21 @@
 import numpy as np
 cimport numpy as np
 cimport cython
+from libc.stdlib cimport malloc
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.nonecheck(False)
 @cython.cdivision(True)
-def CM_system(long double [3]p1, long double [3]p2, long double [3]q1, long double [3]q2):
-	
-	cdef unsigned int i
-	cdef long double [k][3]q_rel
-	cdef long double [k][3]p_rel
+def CM_system(list p1, list p2, list q1, list q2):
 
+    cdef unsigned int i
+    cdef np.ndarray[long double,mode="c",ndim=1] q_rel = np.zeros(3, dtype = np.longdouble)
+    cdef np.ndarray[long double,mode="c",ndim=1] p_rel = np.zeros(3, dtype = np.longdouble)
+    
+    for i in range(3):
+        q_rel[i] = np.array([q1[i] - q2[i]])
+        p_rel[i] = np.array([p1[i] - p2[i]])
+    
+    return (q_rel, p_rel)
 
-	q_rel = np.array([q1[:] - q2[:]])
-	p_rel = np.array([p1[:] - p2[:]])
-	
-	return (q_rel, p_rel)
