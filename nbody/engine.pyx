b7406b8c2eb3e5f1d99c8de5b963e314b9324d55
Francesco Geroni <f.geroni@studenti.unipi.it>
2021-12-01 11:28:47 +0100

    ICN generalized arbitrary iterations

--- nbody/engine.pyx
+++ nbody/engine.pyx
@@ -21,25 +21,22 @@ cdef unsigned int _merge(body_t *bodies, unsigned int nbodies):
 @cython.wraparound(False)
 @cython.nonecheck(False)
 @cython.cdivision(True)
-cdef void _one_step(body_t *bodies, unsigned int nbodies, long double dt, int order) :
+cdef void _one_step(body_t *bodies, unsigned int nbodies, long double dt, int order, int ICN_it):
 
     cdef unsigned int i,j,k
     cdef long double dt2 = 0.5*dt
     cdef body_t b
     cdef body_t tmp_b
+    
+ 
     cdef body_t *mid_point = <body_t *>malloc(nbodies*sizeof(body_t))
     if mid_point == NULL:
         raise MemoryError
         
-    cdef body_t *mid_point_2 = <body_t *>malloc(nbodies*sizeof(body_t))
-
-    if mid_point_2 == NULL:
-        raise MemoryError
-
-    cdef long double **g = <long double **>malloc(nbodies*sizeof(long double *))
+    cdef long double **g = <long double **>malloc(nbodies*sizeof(long double *))    
     if g == NULL:
         raise MemoryError
-    
+                   
     for i in range(nbodies):
         g[i] = <long double *>malloc(6*sizeof(long double))#FIXME: for the spins
         if g[i] == NULL:
@@ -49,50 +46,32 @@ cdef void _one_step(body_t *bodies, unsigned int nbodies, long double dt, int or
 
     _gradients(g, bodies, nbodies, order)
 
+    for i in range(ICN_it):   
     # FIXME: spins are not evolving!
-    # iteration 0
-    for i in range(nbodies):
-        mass = bodies[i].mass
-        mid_point[i].mass = mass
-        
-        for j in range(3):
-
-            tmp_b.q[j] = bodies[i].q[j] + dt2*g[i][3+j]
-            mid_point[i].q[j] = 0.5*(tmp_b.q[j] + bodies[i].q[j])
-
-            tmp_b.p[j] = bodies[i].p[j] - dt2*g[i][j]
-            mid_point[i].p[j] = 0.5*(tmp_b.p[j] + bodies[i].p[j])
-
-            tmp_b.s[j] = bodies[i].s[j]
-            bodies[i].s[j] = 0.5*(tmp_b.s[j] + bodies[i].s[j])
-
-    # update the gradient
-    for i in range(nbodies):
-        memset(g[i], 0, 6*sizeof(long double))
-    _gradients(g, mid_point, nbodies, order)
-    # iteration 1
-    for i in range(nbodies):
-        
-        mass = bodies[i].mass
-        mid_point_2[i].mass = mass
-        
-        for j in range(3):
-
-            tmp_b.q[j] = bodies[i].q[j] + dt2*g[i][3+j]
-            mid_point_2[i].q[j] = 0.5*(tmp_b.q[j] + bodies[i].q[j])
-
-            tmp_b.p[j] = bodies[i].p[j] - dt2*g[i][j]
-            mid_point_2[i].p[j] = 0.5*(tmp_b.p[j] + bodies[i].p[j])
-
-            tmp_b.s[j] = bodies[i].s[j]
-            mid_point_2[i].s[j] = 0.5*(tmp_b.s[j] + bodies[i].s[j])
-
     
-    # update the gradient
-    for i in range(nbodies):
-        memset(g[i], 0, 6*sizeof(long double))
-    _gradients(g, mid_point_2, nbodies, order)
+        for i in range(nbodies):
+            mass = bodies[i].mass
+            mid_point[i].mass = mass
+        
+            for j in range(3):
 
+                tmp_b.q[j] = bodies[i].q[j] + dt2*g[i][3+j]
+                mid_point[i].q[j] = 0.5*(tmp_b.q[j] + bodies[i].q[j])
+
+                tmp_b.p[j] = bodies[i].p[j] - dt2*g[i][j]
+                mid_point[i].p[j] = 0.5*(tmp_b.p[j] + bodies[i].p[j])
+
+                tmp_b.s[j] = bodies[i].s[j]
+                bodies[i].s[j] = 0.5*(tmp_b.s[j] + bodies[i].s[j])
+
+    # update the gradient
+        for i in range(nbodies):
+            memset(g[i], 0, 6*sizeof(long double))
+        _gradients(g, mid_point, nbodies, order)
+        
+        #print(g[0][0])
+        
+    #calculate the final forward coordinates
     for i in range(nbodies):
         mass = bodies[i].mass
         for j in range(3):
@@ -101,7 +80,6 @@ cdef void _one_step(body_t *bodies, unsigned int nbodies, long double dt, int or
 #            bodies[i].s[j] =  dtsquare*g[i,j] #FIXME: spin evolution
     
     _free(mid_point)
-    _free(mid_point_2)
     for i in range(nbodies):
         free(g[i])
  
@@ -128,7 +106,7 @@ cdef void _initialise(body_t *bodies,
     _create_system(bodies, n, mass, x, y, z, px, py, pz, sx, sy, sz)
     return 
 
-cdef void _free(body_t *s) nogil:
+cdef void _free(body_t *s):
     free(<void *>s)
     return
 
@@ -146,7 +124,7 @@ def run(unsigned int nsteps, long double dt, int order,
           np.ndarray[long double, mode="c", ndim=1] pz,
           np.ndarray[long double, mode="c", ndim=1] sx,
           np.ndarray[long double, mode="c", ndim=1] sy,
-          np.ndarray[long double, mode="c", ndim=1] sz):
+          np.ndarray[long double, mode="c", ndim=1] sz, int ICN_it):
     
     from tqdm import tqdm
     cdef unsigned int i,j
@@ -171,12 +149,15 @@ def run(unsigned int nsteps, long double dt, int order,
         # check for mergers
         n = _merge(bodies, n)
         # evolve forward in time
-        _one_step(bodies, n, dt, order)
+        _one_step(bodies, n, dt, order, ICN_it)
         # store 1 every 10 steps
-        solution.append([bodies[i] for i in range(n)])
-        h, t, v = _hamiltonian(bodies, n, order)
-        H.append(h)
-        T.append(t)
-        V.append(v)
+        
+        if i%10 == 0:
+            solution.append([bodies[i] for i in range(n)])
+            h, t, v = _hamiltonian(bodies, n, order)
+        
+            H.append(h)
+            T.append(t)
+            V.append(v)
                      
-    return solution,H, T, V
+    return solution, H, T, V
