Fixes and non-filtered rountine for perihelion shift calculation

nbody/EnergyTest.py
@@ -16,21 +16,21 @@ G = 6.67e-11 #*(u.meter**3)/(u.kilogram*u.second**2) # 6.67e-11 #
 C = 299792458. #*(u.meter/u.second) #299792458. #
 
 Mmerc = 0.3301e24
-Mearth = 5.97216787e24 
+Mearth = 5.97216787e24
 AU = 149597870700. #*u.meter
 Ms = 1.98840987e30
 
 #parameters for solution files management     
-plot_step = 5000
-buffer_lenght = 10000000 # buffer_lenght >= plot_step*data_thin
-data_thin = 1000
+plot_step = 1000
+buffer_lenght = 5000000 # buffer_lenght >= plot_step*data_thin
+data_thin = 10
 
 ICN_it = 2 
 
 #nbodies = 6
-dt = 0.1
+dt = 0.5
 dt2 = 0.5*dt
-N  = 700000000
+N  = 500000000
 p = 0
 
 Neff = int(N/(data_thin*plot_step))
@@ -312,7 +312,7 @@ def energy_test(N, dt, m, x, y, z, px, py, pz, sx, sy, sz, nout, planet_names):
 		r_N = np.sqrt(q_N_rel[:,0]*q_N_rel[:,0] + q_N_rel[:,1]*q_N_rel[:,1] + q_N_rel[:,2]*q_N_rel[:,2])
 		r_1PN = np.sqrt(q_1PN_rel[:,0]*q_1PN_rel[:,0] + q_1PN_rel[:,1]*q_1PN_rel[:,1] + q_1PN_rel[:,2]*q_1PN_rel[:,2])
 			
-		col_rainbow = cm.rainbow(np.linspace(0, 1, len(masses)))   
+		col_rainbow = cm.rainbow(np.linspace(0, 1, len(m)))   
 
 
 		D_tot_q_peri = np.array([[0 for j in range(3)] for i in range(len(peri_indexes_N))]).astype(np.longdouble)
@@ -332,7 +332,7 @@ def energy_test(N, dt, m, x, y, z, px, py, pz, sx, sy, sz, nout, planet_names):
 		f = plt.figure(figsize=(16,14))
 
 		ax1 = f.add_subplot(131)
-		ax1.plot(N_arr, abs(r_spher_1PN - r_spher_N), label = '1PN vs. N')
+		ax1.plot(N_arr, r_spher_1PN - r_spher_N, label = '1PN vs. N')
 		ax1.set_xlabel('iteration', fontsize="x-large")
 		ax1.set_ylabel('Relative radius difference [m]', fontsize="x-large")
 		plt.grid()
@@ -414,9 +414,7 @@ def energy_test(N, dt, m, x, y, z, px, py, pz, sx, sy, sz, nout, planet_names):
 		ax2.grid()
 		ax2.legend(fontsize="large")		
 		
-		plt.show()
-	
-		
+		plt.show()	
 
 		p_s_N = np.sum(phi_shift_N)
 		p_s_N = p_s_N/Neff 
@@ -434,6 +432,7 @@ def energy_test(N, dt, m, x, y, z, px, py, pz, sx, sy, sz, nout, planet_names):
 		print("Standard GR shift: {} [rad/rev]".format(a_shift_N))
 		print("Shift difference: {} [rad/rev]".format(abs(p_s_1PN - p_s_N)))
 		print("Shift difference (test): {} +- {} [rad/rev]".format(abs(phi_shift_test_1PN - phi_shift_test_N), np.sqrt(D_shift_1PN*D_shift_1PN + D_shift_N*D_shift_N)))
+		print("Shift 1PN (test): {} +- {} [rad/rev]\nShift N (test): {} +- {} [rad/rev]".format(phi_shift_test_1PN, D_shift_1PN, phi_shift_test_N, D_shift_N))
 
 		
 	#N body case	   		
@@ -502,7 +501,7 @@ def energy_test(N, dt, m, x, y, z, px, py, pz, sx, sy, sz, nout, planet_names):
 		r_N = np.sqrt(q_N_rel[:,0]*q_N_rel[:,0] + q_N_rel[:,1]*q_N_rel[:,1] + q_N_rel[:,2]*q_N_rel[:,2])
 		r_1PN = np.sqrt(q_1PN_rel[:,0]*q_1PN_rel[:,0] + q_1PN_rel[:,1]*q_1PN_rel[:,1] + q_1PN_rel[:,2]*q_1PN_rel[:,2])
 			
-		col_rainbow = cm.rainbow(np.linspace(0, 1, len(masses)))   
+		col_rainbow = cm.rainbow(np.linspace(0, 1, len(m)))   
 
 
 		D_tot_q_peri = np.array([[0 for j in range(3)] for i in range(len(peri_indexes_N))]).astype(np.longdouble)
@@ -824,7 +823,7 @@ def energy_test(N, dt, m, x, y, z, px, py, pz, sx, sy, sz, nout, planet_names):
 					
 	return 
 	
-
+'''
 #natural initial coordinates
   
 t0 = Time('2001-01-01T0:0:00.0', scale='tdb')
@@ -882,9 +881,9 @@ vcm = np.array([np.sum(vx*m/Mtot), np.sum(vy*m/Mtot), np.sum(vz*m/Mtot)])
 sx = np.zeros(len(m)).astype(np.longdouble)
 sy = np.zeros(len(m)).astype(np.longdouble)
 sz = np.zeros(len(m)).astype(np.longdouble)
-
-
 '''
+
+#'''
 #custom initial coordinates
 m = np.array((2,1)).astype(np.longdouble)
 
@@ -916,7 +915,7 @@ sy[0], sy[1] = 0., 0.
 sz[0], sz[1] = 0., 0.
 
 planet_names = ['sun', 'mercury']
-'''
+#'''
 
 '''
 m[0], m[1] = 2.e-1*Ms, 0.8e-2*Ms

nbody/Kep_dynamic.py
@@ -86,7 +86,7 @@ def gen_3d_frame(p1, p2, q1, q2, Neff, m):
 	return(q1, q2, p1, p2)#, H) 
 '''
 		
-def kepler(q1, q2, p1, p2, N, Neff, H, m, dt, order):
+def kepler(q1, q2, p1, p2, D, N, Neff, H, m, dt, order, q_peri):
 	
 	q_rel, p_rel, q_cm, p_cm = CM_system(p1, p2, q1, q2, Neff, m[0], m[1])	
 	
@@ -318,37 +318,65 @@ def kepler(q1, q2, p1, p2, N, Neff, H, m, dt, order):
         
         	shift = math.sqrt(G*M*R[i])/(r_rel[i]*r_rel[i]) #in keplerian motion this is constant (2nd law)
         	phi_shift[i] = shift
-	
+
+	'''
 	peri_indexes = argrelextrema(r_rel, np.less)
 	peri_indexes = np.transpose(peri_indexes)
 		
 	n_peri = len(peri_indexes)
 	
 	q_peri = np.array([[0 for i in range(0, 3)] for n_peri in range(0, n_peri)], dtype='float64')
+
+	v_peri = np.array([[0 for i in range(0, 3)] for n_peri in range(0, n_peri)], dtype='float64')
+        '''
+
+	n_peri = len(q_peri)
+	
+	#Dq_shift_tmp2 = np.zeros(3, dtype='float64')
+	#Dq_shift_tmp1 = np.array([[0 for i in range(0, 3)] for n_peri in range(0, n_peri-1)], dtype='float64')	
+	
+	#diff = np.zeros(n_peri-1)
 	
 	phi_shift_test = 0 
-	Dq_shift = 0	
+	Dq_shift = 0
+
+
+	test = np.ones((n_peri), dtype='float64')
+	acos_test = np.zeros((n_peri), dtype='float64')
+
 
 	if (n_peri != 0):
 		for i in range(0, n_peri):
 	
-			q_peri[i,:] = q[1, peri_indexes[i], :] #- q_cm[peri_indexes[i], :]
+			#q_peri[i,:] = q2[peri_indexes[i], :] #- q_cm[peri_indexes[i], :]
+			#v_peri[i,:] = p2[peri_indexes[i], :]/m[1] #- q_cm[peri_indexes[i], :]
 			
-			if (i != 0):					
-				Dx = D[peri_indexes[i], 1, 0:3] 
-				Dy = D[peri_indexes[i-1], 1, 0:3]			
+			if (i != 0):		
+				#Dx = D[peri_indexes[i], 1, 0:3] 
+				#Dy = D[peri_indexes[i-1], 1, 0:3]
 			
+				#Dx = D[peri_indexes[i], 1, 3:6] 
+				#Dy = D[peri_indexes[i-1], 1, 3:6]
+			
+				print(math.acos(np.dot(q_peri[i, :], q_peri[i-1, :])/(np.linalg.norm(q_peri[i, :])*np.linalg.norm(q_peri[i-1, :]))), np.dot(q_peri[i, :], q_peri[i-1, :])/(np.linalg.norm(q_peri[i, :])*np.linalg.norm(q_peri[i-1, :])))	
+	
+				test[i] = (np.dot(q_peri[i, :], q_peri[i-1, :])/(np.linalg.norm(q_peri[i, :])*np.linalg.norm(q_peri[i-1, :])))	                        
+
 				phi_shift_test += np.float(math.acos(np.dot(q_peri[i, :], q_peri[i-1, :])/(np.linalg.norm(q_peri[i, :])*np.linalg.norm(q_peri[i-1, :]))))
 
-				Dq_shift_tmp1[i-1, :] = (Dx[:]*np.abs(- (math.tan(np.dot(q_peri[i, :], q_peri[i-1, :]))*np.abs(q_peri[i, :]))/(q_peri[i, :]*q_peri[i, :]*np.abs(q_peri[i-1, :])))) + (Dy[:]*np.abs(- (math.tan(np.dot(q_peri[i, :], q_peri[i-1, :]))*np.abs(q_peri[i-1, :]))/(q_peri[i-1, :]*q_peri[i-1, :]*np.abs(q_peri[i, :]))))
+				acos_test[i] = phi_shift_test
+
+				#phi_shift_test += math.acos(np.dot(v_peri[i, :], v_peri[i-1, :])/(np.linalg.norm(v_peri[i, :])*np.linalg.norm(v_peri[i-1, :])))
+
+				#Dq_shift_tmp1[i-1, :] = (Dx[:]*np.abs(- (math.tan(np.dot(q_peri[i, :], q_peri[i-1, :]))*np.abs(q_peri[i, :]))/(q_peri[i, :]*q_peri[i, :]*np.abs(q_peri[i-1, :])))) + (Dy[:]*np.abs(- (math.tan(np.dot(q_peri[i, :], q_peri[i-1, :]))*np.abs(q_peri[i-1, :]))/(q_peri[i-1, :]*q_peri[i-1, :]*np.abs(q_peri[i, :]))))
+
+				#Dq_shift_tmp1[i-1, :] = (Dx[:]*np.abs(- (math.tan(np.dot(v_peri[i, :], v_peri[i-1, :]))*np.abs(v_peri[i, :]))/(v_peri[i, :]*v_peri[i, :]*np.abs(v_peri[i-1, :])))) + (Dy[:]*np.abs(- (math.tan(np.dot(v_peri[i, :], v_peri[i-1, :]))*np.abs(v_peri[i-1, :]))/(v_peri[i-1, :]*v_peri[i-1, :]*np.abs(v_peri[i, :]))))
 			
-		phi_shift_test = phi_shift_test/n_peri
 
+		#Dq_shift_tmp2[:] = np.sum(Dq_shift_tmp1[:])
+		#Dq_shift = np.linalg.norm(Dq_shift_tmp2[:])
 
-		Dq_shift_tmp2[:] = np.sum(Dq_shift_tmp1[:])
-		Dq_shift = np.linalg.norm(Dq_shift_tmp2[:])
-
-		#print(Dq_shift, Dq_shift_sigma)
+		 #print(Dq_shift, Dq_shift_sigma)
 
 	'''
 	N_arr = np.linspace(0, N - N/Neff, Neff)
@@ -370,7 +398,21 @@ def kepler(q1, q2, p1, p2, N, Neff, H, m, dt, order):
 	plt.show()         
 	'''
 
-	return (r_dif, q_analit_rel, r_kepler, L, a_p, P_quad,  q_peri, phi_shift, phi_shift_test, peri_indexes, Dq_shift)
+	N_arr = np.linspace(0, N, n_peri)
+	        
+	f = plt.figure(figsize=(6,4))
+	ax = f.add_subplot(111)
+	ax.plot(N_arr, test, label ='argument')
+	ax.plot(N_arr, acos_test, label ='acos(argument)')
+	ax.set_xlabel('iteration')
+	ax.set_ylabel('shift')
+	plt.grid()
+	plt.legend()
+	plt.show()   
+
+	return (r_dif, q_analit_rel, r_kepler, L, a_p, P_quad, phi_shift, phi_shift_test)
+
+	#return (r_dif, q_analit_rel, r_kepler, L, a_p, P_quad, q_peri, phi_shift, peri_indexes, phi_shift_test, Dq_shift)
 
 
 	
@@ -502,6 +544,7 @@ def kepler_sol_sys(p, q, D, Neff, H, m, dt, order):
 		
 	n_peri = len(peri_indexes)
 	q_peri = np.array([[0 for i in range(0, 3)] for n_peri in range(0, n_peri)], dtype='float64')
+	v_peri = np.array([[0 for i in range(0, 3)] for n_peri in range(0, n_peri)], dtype='float64')
 	Dq_shift_tmp2 = np.zeros(3, dtype='float64')
 	Dq_shift_tmp1 = np.array([[0 for i in range(0, 3)] for n_peri in range(0, n_peri-1)], dtype='float64')	
 	
@@ -515,7 +558,9 @@ def kepler_sol_sys(p, q, D, Neff, H, m, dt, order):
 
 			#q_peri[i,:] = q_rel[peri_indexes[i], :] #- q_cm[peri_indexes[i], :]
 
-			q_peri[i,:] = q[1, peri_indexes[i], :] #- q_cm[peri_indexes[i], :]
+			q_peri[i,:] = q[1, peri_indexes[i], :]
+
+			#v_peri[i,:] = p[1, peri_indexes[i], :]/m[1] #- q_cm[peri_indexes[i], :]
 			
 			if (i != 0):					
 				Dx = D[peri_indexes[i], 1, 0:3] 
@@ -523,7 +568,13 @@ def kepler_sol_sys(p, q, D, Neff, H, m, dt, order):
 			
 				phi_shift_test += np.float(math.acos(np.dot(q_peri[i, :], q_peri[i-1, :])/(np.linalg.norm(q_peri[i, :])*np.linalg.norm(q_peri[i-1, :]))))
 
+				print(math.acos(np.dot(q_peri[i, :], q_peri[i-1, :])/(np.linalg.norm(q_peri[i, :])*np.linalg.norm(q_peri[i-1, :]))), np.dot(q_peri[i, :], q_peri[i-1, :])/(np.linalg.norm(q_peri[i, :])*np.linalg.norm(q_peri[i-1, :])))
+
+				#phi_shift_test += math.acos(np.dot(v_peri[i, :], v_peri[i-1, :])/(np.linalg.norm(v_peri[i, :])*np.linalg.norm(v_peri[i-1, :])))
+
 				Dq_shift_tmp1[i-1, :] = (Dx[:]*np.abs(- (math.tan(np.dot(q_peri[i, :], q_peri[i-1, :]))*np.abs(q_peri[i, :]))/(q_peri[i, :]*q_peri[i, :]*np.abs(q_peri[i-1, :])))) + (Dy[:]*np.abs(- (math.tan(np.dot(q_peri[i, :], q_peri[i-1, :]))*np.abs(q_peri[i-1, :]))/(q_peri[i-1, :]*q_peri[i-1, :]*np.abs(q_peri[i, :]))))
+
+				#Dq_shift_tmp1[i-1, :] = (Dx[:]*np.abs(- (math.tan(np.dot(v_peri[i, :], v_peri[i-1, :]))*np.abs(v_peri[i, :]))/(v_peri[i, :]*v_peri[i, :]*np.abs(v_peri[i-1, :])))) + (Dy[:]*np.abs(- (math.tan(np.dot(v_peri[i, :], v_peri[i-1, :]))*np.abs(v_peri[i-1, :]))/(v_peri[i-1, :]*v_peri[i-1, :]*np.abs(v_peri[i, :]))))
 			
 		phi_shift_test = phi_shift_test/n_peri
 

nbody/Sol_sys_dyn.py
@@ -23,16 +23,16 @@ Ms = 1.98840987e30
 
 
 #parameters for solution files management     
-plot_step = 5000
-buffer_lenght = 10000000 # buffer_lenght >= plot_step*data_thin
-data_thin = 2000
+plot_step = 500
+buffer_lenght = 1000000 # buffer_lenght >= plot_step*data_thin
+data_thin = 100
 
 PN_order = 0
 ICN_it = 2 
 
 #nbodies = 6
-dt = 0.2
-N  = 600000000
+dt = 1
+N  = 1000000
 
 dt2 = 0.5*dt
 p = 0
@@ -42,7 +42,7 @@ nout = int(N/buffer_lenght) #number of files generated
 
 #---------------------------------------#
 #actual natural initial coordinates 
-t0 = Time('2005-01-01T0:0:00.0', scale='tdb')
+t0 = Time('1995-01-01T0:0:00.0', scale='tdb')
 
 masses = {
 'Sun'     : Ms, #1st planet has to be the central attractor
@@ -53,7 +53,7 @@ masses = {
 'Mars'    : 0.1075*Mearth,
 'Jupiter' : 317.83*Mearth,
 'Saturn'  : 95.16*Mearth,
-'Uranus'  : 14.54*Mearth,
+#'Uranus'  : 14.54*Mearth,
 #'Neptune' : 17.15*Mearth,
 #'Pluto'   : 0.00218*Mearth,
 }
@@ -67,7 +67,7 @@ planet_names = [
 'Mars',
 'Jupiter',
 'Saturn',
-'Uranus',
+#'Uranus',
 #'Neptune',
 #'Pluto',
 ]
@@ -126,7 +126,7 @@ sy_0  = np.zeros(len(m)).astype(np.longdouble)
 sz_0 = np.zeros(len(m)).astype(np.longdouble)
 
 for k in range(len(m)):
-    planets_0 = (get_body_barycentric_posvel(planet_names[k], t0, ephemeris= 'DE440'))
+    planets_0 = (get_body_barycentric_posvel(planet_names[k], t0, ephemeris= 'DE430'))
          
     x_0[k] = planets_0[0].x.to(u.meter).value 
     y_0[k] = planets_0[0].y.to(u.meter).value 
@@ -189,8 +189,8 @@ t_sim = np.concatenate((t_sim[:]), axis=0)
 
 #print(np.shape(s))
 
-
 #print(Neff, np.shape(t_sim), len(V))
+
 #collect SS data
 for i in range(Neff):
     #print(len(t_sim), t_sim[i], i)
@@ -198,7 +198,7 @@ for i in range(Neff):
 
     for planet in planet_names:
 
-        planets.append(get_body_barycentric_posvel(planet, t, ephemeris='DE440'))   
+        planets.append(get_body_barycentric_posvel(planet, t, ephemeris='DE430'))   
 
     x[i][:] = np.array([planet[0].x.to(u.meter).value for planet in planets[len(m)*(i) : len(m)*(i+1)]]).astype(np.longdouble)
     y[i][:] = np.array([planet[0].y.to(u.meter).value for planet in planets[len(m)*(i) : len(m)*(i+1)]]).astype(np.longdouble)
@@ -222,11 +222,12 @@ for i in range(Neff):
 
     #print(t, (t - t0).sec, (i)*(N/Neff)*dt2, i)
     
-t_s = (N)*dt2 #sec
+t_s = (N - N/Neff)*dt2 #sec
 t_s = t_s/(60*60*24*365) #year
 t_nat = (t_sim[-1])/(60*60*24*365) # sec --> year
 
-print('Simulation time: nominal = {} - ICN condition = {}'.format(t_s, t_nat)) #, len(t_nat), len(t_sim))
+print('Simulation time: Nominal = {} - Effective = {}'.format(t_s, t_nat)) #, len(t_nat), len(t_sim))
+
 
 
 N_arr = np.linspace(0, N, Neff)
@@ -258,6 +259,18 @@ ax.set_ylabel('Energy', fontsize="x-large")
 ax.grid()
 plt.legend(fontsize="large")
 plt.show()
+
+'''
+f = plt.figure(figsize=(16,14))
+ax = f.add_subplot(111)
+ax.plot(N_arr, t_sim, label = 't_sim')
+ax.plot(N_arr, np.linspace(0, dt2*N, Neff), label = 't_astro')
+ax.set_xlabel('iteration', fontsize="x-large")
+ax.set_ylabel('time', fontsize="x-large")
+ax.grid()
+plt.legend(fontsize="large")
+plt.show()
+'''
 
 f = plt.figure(figsize=(16,14))
 ax = f.add_subplot(111, projection = '3d')

nbody/nbodysimulation.py
@@ -6,9 +6,10 @@ from nbody.engine import run, _H_2body
 from Kep_dynamic import kepler, kepler_sol_sys, KepElemToCart
 from collections import deque 
 from optparse import OptionParser
-from nbody.CM_coord_system import CM_system
+from nbody.CM_coord_system import CM_system, CartToSpher
 import pickle
-            
+from scipy.signal import argrelextrema
+
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from mpl_toolkits import mplot3d
@@ -188,7 +189,6 @@ if __name__=="__main__":
 
     #print(x,y,z,vx,vy,vz,sx,sy,sz)
     '''
- 
 
     #custom initial coordinates
     m = np.array((2,1)).astype(np.longdouble)
@@ -205,7 +205,7 @@ if __name__=="__main__":
     sy = np.array((2,1)).astype(np.longdouble)
     sz = np.array((2,1)).astype(np.longdouble)
     
-
+    #'''
     m[0], m[1] = 1.e0*Ms, 1.e0*Mmerc
     
     x[0], x[1] =  0., 69.818e9
@@ -219,7 +219,23 @@ if __name__=="__main__":
     sx[0], sx[1] = 0., 0.
     sy[0], sy[1] = 0., 0.
     sz[0], sz[1] = 0., 0.
+    #'''
 
+    '''
+    m[0], m[1] = 1.e0*Ms, 1.e0*Mearth
+    
+    x[0], x[1] =  0., 152.100e9
+    y[0], y[1] = 0., 0.
+    z[0], z[1] = 0., 0.
+
+    vx[0], vx[1] = 0., 0.
+    vy[0], vy[1] = 0., 29.29e3
+    vz[0], vz[1] = 0., 0.
+    
+    sx[0], sx[1] = 0., 0.
+    sy[0], sy[1] = 0., 0.
+    sz[0], sz[1] = 0., 0.
+    '''
 
     '''
     #random initial coordinates
@@ -242,9 +258,9 @@ if __name__=="__main__":
     
     #parameters for solution files management 
   
-    plot_step = 2500
-    buffer_lenght = 500000
-    data_thin = 100
+    plot_step = 1000000
+    buffer_lenght = 1000000
+    data_thin = 5
 
     #------------------------------------------------------#  
  
@@ -261,10 +277,11 @@ if __name__=="__main__":
         run(N, np.longdouble(dt), opts.PN_order, m, x, y, z, m*vx, m*vy, m*vz, sx, sy, sz, ICN_it, data_thin, buffer_lenght, plot_step)
     
     s, H, T, V, D, t_sim = [], [], [], [], [], []
+    s_long, D_long = [], [] #, H_long, T_long, V_long, t_sim_long = [], [], [], []
     
     for i in range(nout):  
         s_tot, H_tot, T_tot, V_tot, t_tot, D_tot = [], [], [], [], [], []
-        
+
         s_tot.append(pickle.load(open('solution_{}_order{}.pkl'.format(i, order),'rb')))
         H_tot.append(pickle.load(open('hamiltonian_{}_order{}.pkl'.format(i, order),'rb')))
         T_tot.append(pickle.load(open('kinetic_{}_order{}.pkl'.format(i, order),'rb')))
@@ -278,6 +295,13 @@ if __name__=="__main__":
         V.append(V_tot[0][::plot_step])
         D.append(D_tot[0][::plot_step])
         t_sim.append(t_tot[::plot_step])        
+
+        s_long.append(s_tot[0]) #[::plot_step])
+        #H_long.append(H_tot[0]) #[::plot_step])
+        #T_long.append(T_tot[0]) #[::plot_step])
+        #V_long.append(V_tot[0]) #[::plot_step])
+        D_long.append(D_tot[0]) #[::plot_step])
+        #t_sim_long.append(t_tot) #[::plot_step])    
         
         del s_tot
         del H_tot
@@ -304,6 +328,21 @@ if __name__=="__main__":
     D = np.concatenate((D[:]), axis=0)
     t_sim = np.concatenate((t_sim[:]), axis=0) 
 
+    #
+    s_long = np.array(s_long, dtype=object)#.flatten()
+    #H_long = np.array(H_long, dtype=object)#.flatten()
+    #T_long = np.array(T_long, dtype=object)#.flatten()
+    #V_long = np.array(V_long, dtype=object)#.flatten()
+    D_long = np.array(D_long, dtype=object)
+    #t_sim_long = np.array(t_sim_long, dtype=object)
+    
+    s_long = np.concatenate((s_long[:]))
+    #H_long = np.concatenate((H_long[:]))
+    #T_long = np.concatenate((T_long[:]))
+    #V_long = np.concatenate((V_long[:]))
+    D_long = np.concatenate((D_long[:]), axis=0)
+    #t_sim_long = np.concatenate((t_sim_long[:]), axis=0) 
+    #
     
     if opts.animate == 1:
         
@@ -377,7 +416,7 @@ if __name__=="__main__":
 
         f = plt.figure(figsize=(6,4))
         ax = f.add_subplot(111)
-        ax.plot(range(Neff), nbodies)
+        ax.plot(N_arr, nbodies)
         ax.set_xlabel('iteration')
         ax.set_ylabel('nbodies')
     
@@ -477,7 +516,12 @@ if __name__=="__main__":
             s1 = np.array([[0 for i in range(0, 3)] for Neff in range(0, Neff)], dtype='longdouble')
             q2 = np.array([[0 for i in range(0, 3)] for Neff in range(0, Neff)], dtype='longdouble')
             p2 = np.array([[0 for i in range(0, 3)] for Neff in range(0, Neff)], dtype='longdouble')       
-            s2 = np.array([[0 for i in range(0, 3)] for Neff in range(0, Neff)], dtype='longdouble')    
+            s2 = np.array([[0 for i in range(0, 3)] for Neff in range(0, Neff)], dtype='longdouble')   
+
+            q1_long = np.array([[0 for i in range(0, 3)] for k in range(0, int(N/data_thin))], dtype='longdouble')
+            p1_long = np.array([[0 for i in range(0, 3)] for k in range(0, int(N/data_thin))], dtype='longdouble')
+            q2_long = np.array([[0 for i in range(0, 3)] for k in range(0, int(N/data_thin))], dtype='longdouble')
+            p2_long = np.array([[0 for i in range(0, 3)] for k in range(0, int(N/data_thin))], dtype='longdouble')           
                       
             for i in range(0, Neff):
 
@@ -488,14 +532,41 @@ if __name__=="__main__":
                 q2[i,:] = s[i][1]['q']
                 p2[i,:] = s[i][1]['p']
                 s2[i,:] = s[i][1]['s']                      
+
+            print(len(s[:]), len(s[:]), N, np.shape(q1_long), np.shape(q1))
+
+            for i in range(0, int(N/data_thin)):
+
+                q1_long[i,:] = s_long[i][0]['q']
+                p1_long[i,:] = s_long[i][0]['p']
+
+                q2_long[i,:] = s_long[i][1]['q']
+                p2_long[i,:] = s_long[i][1]['p']
             
-           
+            #
+            q_long_rel, p_long_rel, q_long_cm, p_long_cm = CM_system(p1_long, p2_long, q1_long, q2_long, int(N/data_thin), m[0], m[1])   
+
+            r_long_rel = np.sqrt(q_long_rel[:,0]*q_long_rel[:,0] + q_long_rel[:,1]*q_long_rel[:,1] + q_long_rel[:,2]*q_long_rel[:,2])
+            
+            peri_indexes = argrelextrema(r_long_rel, np.less)
+            peri_indexes = np.transpose(peri_indexes)            
+ 
+            n_peri = len(peri_indexes)
+
+            q_peri_long = np.array([[0 for i in range(0, 3)] for n_peri in range(0, n_peri)], dtype='float64')
+ 
+            if (n_peri != 0):
+                for i in range(0, n_peri):
+                    q_peri_long[i,:] = q2_long[peri_indexes[i], :] 
+            #
+
+
             q_rel, p_rel, q_cm, p_cm = CM_system(p1, p2, q1, q2, Neff, m[0], m[1])        
             r_sim = np.sqrt(q_rel[:,0]*q_rel[:,0] + q_rel[:,1]*q_rel[:,1] + q_rel[:,2]*q_rel[:,2])
 
             N_shift = 6.488703338e-5
             
-            r_dif, q_an_rel, r_kepler, L, a_p, P_quad, phi_shift, q_peri, peri_indexes, phi_shift_test = kepler(q1, q2, p1, p2, N, Neff, H, m, dt, order)
+            r_dif, q_an_rel, r_kepler, L, a_p, P_quad, phi_shift, phi_shift_test = kepler(q1, q2, p1, p2, D_long, N, Neff, H, m, dt, order, q_peri_long) #peri_indexes, Dq_shift
             
                    
             p_s = np.sum(phi_shift)
@@ -589,31 +660,38 @@ if __name__=="__main__":
             
             plt.show()
             '''
+            col_rainbow = cm.rainbow(np.linspace(0, 1, len(q_peri_long)))   
+            
             
             f = plt.figure(figsize=(16,6))
             
             ax = f.add_subplot(111, projection = '3d')
             #ax.plot(q_rel[:,0], q_rel[:,1], q_rel[:,2], label = 'Numerical solution', alpha=0.5)  
-            for i in range(0, len(peri_indexes)): 
-                ax.plot(q_peri[i,0], q_peri[i,1], q_peri[i,2], 'o', label = 'Perihelion orbit {}'.format(i))
+            for i in range(0, len(q_peri_long)): 
+                ax.plot(q_peri_long[i,0], q_peri_long[i,1], q_peri_long[i,2], 'o', label = 'Perihelion orbit {}'.format(i), color = col_rainbow[i])
             ax.set_xlabel('x [m]', fontsize="x-large")
             ax.set_ylabel('y [m]', fontsize="x-large")
             ax.set_zlabel('z [m]', fontsize="x-large")        
             plt.legend(fontsize="large")
             
             plt.show()                  
-
+            
+            
+            N_arr_long = np.linspace(0, N, int(N/data_thin))
+            
+            '''
             f = plt.figure(figsize=(16,6))
             
-            ax = f.add_subplot(111)
-            ax.scatter(N_arr, r_sim, label = 'Orbital radius', alpha=0.5)
             
-            for i in range(0, len(peri_indexes)):
+            ax = f.add_subplot(111)
+            ax.scatter(N_arr_long, r_long_rel, label = 'Orbital radius', alpha=0.5)
+   
+            for i in range(0, len(q_peri_long)):
             	index = peri_indexes[i]
             	index = index[0]
-            	index_N = N_arr[index]
+            	index_N = N_arr_long[index]
 
-            	ax.plot(index_N, r_sim[index], 'o', label = 'Perihelion orbit {}'.format(i))
+            	ax.plot(index_N, r_long_rel[index], 'o', label = 'Perihelion orbit {}'.format(i))
             	
             ax.set_xlabel('iteration', fontsize="x-large")
             ax.set_ylabel('orbital radius [m]', fontsize="x-large")       
@@ -621,12 +699,39 @@ if __name__=="__main__":
             plt.legend(fontsize="large")
             
             plt.show()                  
+            '''
+
+            r_spher_rel, theta_spher_rel, phi_spher_rel= CartToSpher(np.ascontiguousarray(q_rel[:,0]), np.ascontiguousarray(q_rel[:,1]), np.ascontiguousarray(q_rel[:,2]))
+                
+            f = plt.figure(figsize=(16,14))
+            ax1 = f.add_subplot(131)
+            ax1.plot(N_arr, r_spher_rel)
+            ax1.set_xlabel('iteration', fontsize="x-large")
+            ax1.set_ylabel('Relative distance [m]', fontsize="x-large")
+            plt.grid()
+            plt.legend(fontsize="large")
+
+            ax2 = f.add_subplot(132)
+            ax2.plot(N_arr, theta_spher_rel, label = '')
+            ax2.set_xlabel('iteration', fontsize="x-large")
+            ax2.set_ylabel('Righ ascension [rad]', fontsize="x-large")
+            plt.grid()
+            plt.legend(fontsize="large")
+
+            ax3 = f.add_subplot(133)
+            ax3.plot(N_arr, phi_spher_rel, label = '')
+            ax3.set_xlabel('iteration', fontsize="x-large")
+            ax3.set_ylabel('Declination [rad]', fontsize="x-large")
+            plt.grid()
+            plt.legend(fontsize="large")
+
+            plt.show()
+ 
 
             f = plt.figure(figsize=(16,16))
 
             ax1 = f.add_subplot(121)
-            for k in range(len(m)):
-                ax1.plot(N_arr, D[:][k][0], label= "Numerical Error")
+            ax1.plot(N_arr, D[:,1, 0], label= "Numerical Error")
             ax1.set_xlabel('iteration', fontsize="x-large")
             ax1.set_ylabel(r'$\Delta x$ [m]', fontsize="x-large")
             plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
@@ -634,8 +739,7 @@ if __name__=="__main__":
             plt.legend(fontsize="large")
 
             ax2 = f.add_subplot(122)
-            for k in range(len(m)):
-                ax2.plot(N_arr, D[:][k][1], label= "Numerical Error")
+            ax2.plot(N_arr, D[:,1,1], label= "Numerical Error")
             ax2.set_xlabel('iteration', fontsize="x-large")
             ax2.set_ylabel(r'$\Delta y$ [m]', fontsize="x-large")
             plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
@@ -644,13 +748,32 @@ if __name__=="__main__":
 
             plt.show()       
 
-      
+
+            q2_an = q1 - q_an_rel     
+            
+            D_q2_an = np.array([[0 for i in range(0, 3)] for Neff in range(0, Neff)], dtype='longdouble')#.astype(np.longdouble) 
+
+            for i in range(Neff):
+       
+                D_q2_an[i, 0] = np.sqrt(D[i,0, 0]*D[i,0, 0] + (np.sqrt(D[i,0, 0]*D[i,0, 0] + D[i,1, 0]*D[i,1, 0])))
+                D_q2_an[i, 1] = np.sqrt(D[i,0, 1]*D[i,0, 1] + (np.sqrt(D[i,0, 1]*D[i,0, 1] + D[i,1, 1]*D[i,1, 1])))
+                D_q2_an[i, 2] = np.sqrt(D[i,0, 2]*D[i,0, 2] + (np.sqrt(D[i,0, 2]*D[i,0, 2] + D[i,1, 2]*D[i,1, 2])))
+            
+            f = plt.figure(figsize=(16,6))
+            ax = f.add_subplot(111)
+            ax.errorbar(q2[:,0], q2[:,1], yerr = D[:,1, 1], xerr = D[:,1, 0], label = 'Numerical solution', alpha=0.7)  
+            ax.errorbar(q2_an[:,0], q2_an[:,1], yerr = D_q2_an[:, 1], xerr = D_q2_an[:,0], label = 'Analytical solution' )
+            ax.set_xlabel('x [m]', fontsize="x-large")
+            ax.set_ylabel('y [m]', fontsize="x-large")      
+            plt.legend(fontsize="large")
+            
+    
             print('Newtonian shift {} [rad/rev]'.format(N_shift)) 
             print('Standard GR shift = {} [rad/revolution]'.format(a_shift))
             #print('Perihelion shift = {}'.format(a_p[-1]*415.2))
             
             print('Numerical perihelion shift: {} [rad/revolution]'.format(p_s))
-            print('Numerical perihelion shift (test): {} [rad/revolution]'.format(phi_shift_test))
+            print('Numerical perihelion shift (test): {} +- {} [rad/revolution]'.format(phi_shift_test, 0)) #Dq_shift))
             
         else :
 
@@ -766,13 +889,14 @@ if __name__=="__main__":
             
             plt.show()
             
-            
+            col_rainbow = cm.rainbow(np.linspace(0, 1, len(q_peri)))   
+         
             f = plt.figure(figsize=(16,6))
             
             ax = f.add_subplot(111, projection = '3d')
             ax.scatter(q[1,:,0], q[1,:,1], q[1,:,2], label = 'Numerical solution', alpha=0.5)
             for i in range(0, len(q_peri)): 
-            	ax.plot(q_peri[i,0], q_peri[i,1], q_peri[i,2], 'o', label = 'Perihelion orbit {}'.format(i))
+            	ax.plot(q_peri[i,0], q_peri[i,1], q_peri[i,2], 'o', label = 'Perihelion orbit {}'.format(i), color = col_rainbow[i])
             ax.set_xlabel('x [m]')
             ax.set_ylabel('y [m]')
             ax.set_zlabel('z [m]')        